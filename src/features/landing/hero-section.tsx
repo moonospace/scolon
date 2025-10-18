dfd7b94fe28ad37724b2100db4486668f85d3f6b
Nyoman Sunima <nyomansunima@gmail.com>
2025-10-18 20:05:39 +0800

    style: check and lint code

--- src/features/landing/hero-section.tsx
+++ src/features/landing/hero-section.tsx
@@ -1,10 +1,9 @@
-import { Link } from "react-router"
-import logo from "./logo.png"
+import logo from "./logo.png";
 
-interface ItemData {
-  label: string
-  url: string
-}
+type ItemData = {
+  label: string;
+  url: string;
+};
 
 const items: ItemData[] = [
   {
@@ -23,38 +22,39 @@ const items: ItemData[] = [
     label: "Linkedin newsletter",
     url: "https://www.linkedin.com/newsletters/the-scolon-7359456360293453826",
   },
-]
+];
 
 export function HeroSection() {
   return (
-    <section className="flex flex-col py-28 items-center">
+    <section className="flex flex-col items-center py-28">
       <img
+        alt="logo"
+        className="transition-all duration-300 hover:scale-95"
+        height={75}
         src={logo}
         width={75}
-        height={75}
-        className="transition-all duration-300 hover:scale-95"
       />
 
-      <h2 className="text-4xl font-bold font-heading leading-tight text-center mt-10">
+      <h2 className="mt-10 text-center font-bold font-heading text-4xl leading-tight">
         Level up your
         <br />
         devs skills
       </h2>
 
-      <p className="leading-7 text-foreground/60 text-center mt-6 laptop:w-10/12">
+      <p className="mt-6 laptop:w-10/12 text-center text-foreground/60 leading-7">
         A space for developer to learn more about development, design
         applications, growing dev skills and sharing tips and tricks to become
         better developer.
       </p>
 
-      <div className="flex flex-col mt-10 gap-1">
+      <div className="mt-10 flex flex-col gap-1">
         {items.map((item, i) => (
-          <Link to={item.url} key={i} className="flex items-center gap-2 py-2">
-            <i className="fi fi-sc-bullet text-sm text-foreground/40" />
+          <a className="flex items-center gap-2 py-2" href={item.url} key={i}>
+            <i className="fi fi-sc-bullet text-foreground/40 text-sm" />
             <span>{item.label}</span>
-          </Link>
+          </a>
         ))}
       </div>
     </section>
-  )
+  );
 }
