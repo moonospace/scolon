7087104bcda3359b86508e3dd05b0f6394922e43
Nyoman Sunima <nyomansunima@gmail.com>
2025-12-03 16:37:26 +0800

    refactor: update the root component and layout

--- src/app/routes/index.tsx
+++ src/app/routes/index.tsx
@@ -1,19 +1,20 @@
 import { HeroSection } from "@features/landing";
-import { generatedMetadata } from "@shared/libs";
+import { generatedMetadata } from "@shared/libs/shared-metadata";
 import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/")({
   head: () => ({
     meta: generatedMetadata({
-      title: "Level Up Your Devs Skills | The Scolon",
+      title:
+        "Top Developer Newsletter, Courses, Community & Resources | Scolon",
       description:
         "Top place to learn and growing development skills, career, business and learn to become better developers",
     }),
   }),
-  component: PageComponent,
+  component: RouteComponent,
 });
 
-function PageComponent() {
+function RouteComponent() {
   return (
     <main className="flex flex-col">
       <HeroSection />
